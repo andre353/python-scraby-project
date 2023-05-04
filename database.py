08f5c65373c83621e63460abb0b800c99f313715
andre353 <71174275+andre353@users.noreply.github.com>
2023-05-04 14:41:10 +0000

    Connect to books table

--- database.py
+++ database.py
@@ -1,21 +1,17 @@
 import os
-from sqlalchemy import create_engine
-# from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text
 
-engine = create_engine(f"mysql+pymysql://{os.getenv('USERNAME')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}/{os.getenv('DATABASE')}?charset=utf8mb4")
+db_connection_string = f"mysql+pymysql://{os.getenv('USERNAME')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}/{os.getenv('DATABASE')}?charset=utf8mb4"
 
-# db_connection_string = f"mysql+pymysql://{os.getenv("USERNAME")}:{os.getenv("PASSWORD")}@{os.getenv("HOST")}/{os.getenv("DATABASE")}?charset=utf8mb4"
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  }
+)
 
-
-# engine = create_engine(
-#   db_connection_string,
-#   connect_args={
-#     "ssl": {
-#       "ssl_ca": "/etc/ssl/cert.pem"
-#     }
-#   }
-# )
-
-# with engine.connect() as conn:
-#   result = conn.execute(text("select * from books"))
-#   print(result.all())
+with engine.connect() as conn:
+  result = conn.execute(text("select * from books"))
+  print(result.all())
